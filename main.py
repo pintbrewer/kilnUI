1cfb7561aca8c7368d0f29ac7e3bc26d14f826a2
pintbrewer <pintbrewer>
2018-10-15 21:14:00 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# import the pygame module, so you can use it
+import pygame
+
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT  = 64
+black = (0,0,0)
+white = (255,255,255)
+
+
+def init_screen():
+    pygame.init()
+    # create a surface on screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    screen.fill(black) 
+    pygame.draw.rect(screen, white, (0,0,SCREEN_WIDTH,SCREEN_HEIGHT), 1) # screen border
+
+    pygame.display.update()
+    return screen
+
+def home_screen(screen, selected):
+    selections = ["LOAD SCHEDULE", "NEW SCHEDULE"]
+    main_font = pygame.font.SysFont("menlo", 10)
+    font_height = main_font.get_height()
+    text_start = [2, 2]
+    text = []
+    for item in selections:
+        if selected == selections.index(item):
+            text.append(main_font.render(item, 1, black, white))
+        else:
+            text.append(main_font.render(item, 1, white, black))
+    for txt in text:
+        screen.blit(txt, text_start)
+        text_start = [text_start[0], text_start[1] + font_height]
+    pygame.display.update()
+
+def main():
+    running = True
+    screen = init_screen() 
+    home_screen(screen, 0)
+    # main loop
+    while running:
+        # event handling, gets all event from the eventqueue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            print(event)
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+     
+
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()

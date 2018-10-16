6904440fa57fec3d6b695916511cd32e49be2c53
pintbrewer <pintbrewer>
2018-10-15 21:45:56 -0400

    adding screen logic

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ SCREEN_WIDTH = 128
 SCREEN_HEIGHT  = 64
 black = (0,0,0)
 white = (255,255,255)
-
+HOME = ["LOAD SCHEDULE", "NEW SCHEDULE"]
 
 def init_screen():
     pygame.init()
@@ -18,8 +18,8 @@ def init_screen():
     pygame.display.update()
     return screen
 
-def home_screen(screen, selected):
-    selections = ["LOAD SCHEDULE", "NEW SCHEDULE"]
+def select_mode(screen, selections, selected):
+    #selections = ["LOAD SCHEDULE", "NEW SCHEDULE"]
     main_font = pygame.font.SysFont("menlo", 10)
     font_height = main_font.get_height()
     text_start = [2, 2]
@@ -37,14 +37,22 @@ def home_screen(screen, selected):
 def main():
     running = True
     screen = init_screen() 
-    home_screen(screen, 0)
+    selected = 0
+    select_mode(screen, HOME, selected)
     # main loop
     while running:
         # event handling, gets all event from the eventqueue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
             print(event)
-            if event.type == pygame.QUIT:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                if selected < len(HOME) - 1:
+                    selected = selected + 1
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                if selected > 0 :
+                    selected = selected - 1      
+            select_mode(screen, HOME, selected)
+            if (event.type == pygame.QUIT):
                 # change the value to False, to exit the main loop
                 running = False
      
